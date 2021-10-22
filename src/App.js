f8c79ddcf52453ffab7bf2c876ac70771f1baf2b
suryaa6666 <suryaelidanto@gmail.com>
2021-10-22 22:26:31 +0700

    Trying button mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 function App() {
+  const [quotes, setQuotes] = useState(``);
+
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`${data.content} —${data.author}`);
+      // setQuotes(`${data.content} —${data.author}`);
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1> {quotes} </h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
