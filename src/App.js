d8e7d1a5a7502f73244a063b2279c1601902fb39
suryaa6666 <suryaelidanto@gmail.com>
2021-10-23 05:51:19 +0700

    Centering card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+import { Card, Button, CardContent } from "@mui/material";
+// import styles from 'Styles.js';
 
 function App() {
   const [quotes, setQuotes] = useState(``);
 
-  fetch("https://api.quotable.io/random")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(`${data.content} —${data.author}`);
-      // setQuotes(`${data.content} —${data.author}`);
-    });
+  const GenerateQuotes = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`${data.content} —${data.author}`);
+        setQuotes(`${data.content} —${data.author}`);
+      });
+  };
+
   return (
-    <div className="wrapper">
-      <h1> {quotes} </h1>
-      <Button variant="contained">Hello World</Button>
+    <div>
+      <Card
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%,-50%)",
+        }}
+        sx={{ maxWidth: 500 }}
+      >
+        <CardContent>
+          <h1> {quotes} </h1>
+          <Button variant="contained" onClick={GenerateQuotes}>
+            {" "}
+            Generate Quotes
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
